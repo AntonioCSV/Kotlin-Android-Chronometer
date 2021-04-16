611ea5b55c3b88705d32f8f6fb2e22894e76c490
Antonio Carlos Silva Vasconcelos <a.silva.vasconcelos@avanade.com>
2021-04-16 12:34:32 -0300

    Start, Pause and Clear funcionatilies developed and working

--- app/src/main/java/me/acsv/cronometro/MainActivity.kt
+++ app/src/main/java/me/acsv/cronometro/MainActivity.kt
@@ -2,10 +2,51 @@ package me.acsv.cronometro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import me.acsv.cronometro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var running: Boolean = false
+    private var pausedTime: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iniciar.setOnClickListener {
+            iniciarCronometro()
+        }
+
+        binding.pausar.setOnClickListener {
+            pausarCronometro()
+        }
+
+        binding.zerar.setOnClickListener {
+            zerarCronometro()
+        }
+    }
+
+    private fun iniciarCronometro() {
+        if(!running) {
+            binding.cronometro.base = if(pausedTime > 0) pausedTime else SystemClock.elapsedRealtime()
+            binding.cronometro.start()
+            running = true
+        }
+    }
+
+    private fun pausarCronometro() {
+        if(running) {
+            binding.cronometro.stop()
+            pausedTime = SystemClock.elapsedRealtime()
+            running = false
+        }
+    }
+
+    private fun zerarCronometro() {
+        binding.cronometro.base = SystemClock.elapsedRealtime()
+        pausedTime = 0
     }
 }
